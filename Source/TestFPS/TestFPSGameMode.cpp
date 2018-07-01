182dc2d8893853a73cda45c2345a2ba3dbcb5267
SeverZmith <eric.g.smith.24@gmail.com>
2018-06-30 22:40:03 -0700

    C13 Re-organized entire file hierarchy. Cleaned up redirectors in editor.

--- Source/TestFPS/TestFPSGameMode.cpp
+++ Source/TestFPS/TestFPSGameMode.cpp
@@ -2,14 +2,15 @@
 
 #include "TestFPSGameMode.h"
 #include "TestFPSHUD.h"
-#include "TestFPSCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestFPSGameMode::ATestFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/TestFPSCharacter_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/TestFPS/TestFPSHUD.cpp
+++ Source/TestFPS/TestFPSHUD.cpp
@@ -10,7 +10,8 @@
 ATestFPSHUD::ATestFPSHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	// static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
